b00cfd7f533164dc27ecf304f2b7fea1b240eea6
vgita <22777783+vgita@users.noreply.github.com>
2024-07-02 14:50:18 +0300

    define domain events

--- Services/Ordering/Ordering.Domain/Models/Order.cs
+++ Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -16,4 +16,50 @@ public class Order : Aggregate<OrderId>
         get => _orderItems.Sum(item => item.Price * item.Quantity);
         private set { }
     }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        Order order = new()
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            OrderStatus = OrderStatus.Pending,
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if (orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
--- Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -2,7 +2,8 @@ namespace Ordering.Domain.Models;
 
 public class OrderItem : Entity<OrderItemId>
 {
-    public OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+    //created and managed by the Order aggregate
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
