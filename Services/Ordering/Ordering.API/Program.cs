349ab48640d16c6615dab3b7c74239fee53cefd5
vgita <22777783+vgita@users.noreply.github.com>
2024-07-08 15:25:42 +0300

    order api: health check and custom exception handling

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
+
 
 var app = builder.Build();
 
