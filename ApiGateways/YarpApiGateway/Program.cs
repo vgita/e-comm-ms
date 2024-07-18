308ab74f87c46c81610adb6b6d4321976522dee8
vgita <22777783+vgita@users.noreply.github.com>
2024-07-18 21:02:50 +0300

    configure rate limiting policy

--- ApiGateways/YarpApiGateway/Program.cs
+++ ApiGateways/YarpApiGateway/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(opts =>
+{
+    opts.AddFixedWindowLimiter("fixed", o => //Max 5 requests per 10 seconds
+    {
+        o.Window = TimeSpan.FromSeconds(10);
+        o.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
