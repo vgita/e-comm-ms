f0614b37a5a742cd7afa7b9941cdfac3438d5ced
vgita <22777783+vgita@users.noreply.github.com>
2024-07-20 00:11:21 +0300

    define models in web client

---
+++ Clients/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,23 @@
+namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
+public class ShoppingCartItemModel
+{
+    public int Quantity { get; set; }
+    public string Color { get; set; } = default!;
+    public decimal Price { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = default!;
+}
+
+//wrapper records
+public record GetBasketResponse(ShoppingCartModel Cart);
+public record StoreBasketRequest(ShoppingCartModel Cart);
+public record StoreBasketResponse(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
---
+++ Clients/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,15 @@
+namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Categories { get; set; } = [];
+    public string Description { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
+    public decimal Price { get; set; }
+}
+
+//wrapper records
+public record GetProductResponse(IEnumerable<ProductModel> Products);
+public record GetProductByIdResponse(ProductModel Product);
