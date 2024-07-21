b90c625411d4738469908957d3ef77233c104caa
vgita <22777783+vgita@users.noreply.github.com>
2024-07-21 15:11:53 +0300

    Update OrderName.cs

--- Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -3,7 +3,7 @@ namespace Ordering.Domain.ValueObjects;
 
 public record OrderName
 {
-    private const int DefaultLength = 3;
+    // private const int DefaultLength = 3;
     public string Value { get; }
 
     private OrderName(string value) => Value = value;
@@ -11,7 +11,7 @@ public record OrderName
     public static OrderName Of(string value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(value);
-        ArgumentOutOfRangeException.ThrowIfLessThan(value.Length, DefaultLength);
+        //  ArgumentOutOfRangeException.ThrowIfLessThan(value.Length, DefaultLength);
 
         return new OrderName(value);
     }
